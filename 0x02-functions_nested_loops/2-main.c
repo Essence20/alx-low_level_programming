d1ee2c40aecac8dbaa2cf523b317a75b6a670c1e
Essence20git config --global user.name Essence20 <prosperikiensikimama@gmail.com>
2023-02-21 06:06:17 -0800

    task 2

---
+++ 0x02-functions_nested_loops/2-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - Entry point. Calls print_alphabet_x10 function.
+ *
+ * Return: Always 0 (Success).
+ */
+int main(void)
+{
+	print_alphabet_x10();
+	return (0);
+}
+
