4587ed5212649271c15c49c4deaa6c0f3f68ca00
Essence20git config --global user.name Essence20 <prosperikiensikimama@gmail.com>
2023-04-03 13:10:05 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,19 +1,17 @@
-#include "lists.h"
+include "lists.h"
 
 /**
  * print_listint - prints all the elements of a linked list
  * @h: pointer to the head of the list
  *
  * Return: number of nodes in the list
- **/
+ */
 size_t print_listint(const listint_t *h)
 {
 size_t count = 0;
-while (h != NULL)
+while (h != NULL) 
 {
 printf("%d\n", h->n);
-count++;
+count + 1;
 h = h->next;
-}
-return (count);
 }
